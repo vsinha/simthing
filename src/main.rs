47bd41bb41ca52f69969e60082f002e27fde92f6
Viraj Sinha <virajosinha@gmail.com>
2019-05-22 22:04:59 -0400

    they swim around

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use cgmath::*;
 use ggez::conf::*;
 use ggez::event::{self, EventHandler, EventsLoop, KeyCode, KeyMods};
 use ggez::{Context, ContextBuilder, GameResult};
+use rand::Rng;
 
 fn window_setup() -> (Context, EventsLoop) {
     ContextBuilder::new("simthing", "Ty Overby | Viraj Sinha")
@@ -22,14 +23,22 @@ fn main() {
 }
 
 struct Agent {
-    position: Point2<f32>,
+    position: Vector2<f32>,
     velocity: Vector2<f32>,
+    target: Option<Vector2<f32>>,
 }
 
 struct MyGame {
     circle_mesh: ggez::graphics::Mesh,
+    target_mesh: ggez::graphics::Mesh,
     camera_pos: Vector3<f32>,
     agents: Vec<Agent>,
+}
+
+fn create_point(width: f32, height: f32) -> Vector2<f32> {
+    let mut rng = rand::thread_rng();
+
+    Vector2::new(rng.gen_range(0., width), rng.gen_range(0., height))
 }
 
 impl MyGame {
@@ -43,16 +52,33 @@ impl MyGame {
             0.1,
             Color::from_rgb(255, 0, 0),
         )?;
+
+        let target_mesh = Mesh::new_circle(
+            ctx,
+            DrawMode::Fill(FillOptions::default()),
+            [0.0, 0.0],
+            1.0,
+            0.1,
+            Color::from_rgb(50, 100, 255),
+        )?;
+
         let agents = (0..10).map(|i| {
-            let position = Point2::new(100.0, 100.0);
+            let position = Vector2::new(100.0, 100.0);
             let i = ((i as f32) / 10.0) * 3.14;
             let vx = f32::sin(i as f32);
             let vy = f32::cos(i as f32);
             let velocity = Vector2::new(vx, vy) / 10.0;
-            Agent { position, velocity }
+            let random_target = create_point(100.0, 100.0);
+            let target = Some(random_target);
+            Agent {
+                position,
+                velocity,
+                target,
+            }
         });
         Ok(MyGame {
             circle_mesh,
+            target_mesh,
             camera_pos: Vector3::new(0.0, 0.0, 0.0),
             agents: agents.collect(),
         })
@@ -62,7 +88,18 @@ impl MyGame {
 impl EventHandler for MyGame {
     fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
         for agent in &mut self.agents {
-            agent.position += agent.velocity;
+            if rand::random() {
+                // pick a new target location
+                agent.target = Some(create_point(500.0, 500.0))
+            }
+            match agent.target {
+                None => (),
+                Some(target) => {
+                    let new_direction = Vector2::normalize(target - agent.position);
+                    agent.velocity = Vector2::normalize(agent.velocity + new_direction) * 5.0;
+                    agent.position += agent.velocity;
+                }
+            }
         }
         Ok(())
     }
@@ -98,9 +135,18 @@ impl EventHandler for MyGame {
             draw(
                 ctx,
                 &self.circle_mesh,
-                DrawParam::default().dest(agent.position),
+                DrawParam::default().dest(Point2::from_vec(agent.position)),
             )?;
+
+            agent.target.map(|target| {
+                draw(
+                    ctx,
+                    &self.target_mesh,
+                    DrawParam::default().dest(Point2::from_vec(target)),
+                )
+            });
         }
+
         ggez::timer::fps(ctx);
         present(ctx)?;
         ggez::graphics::pop_transform(ctx);
